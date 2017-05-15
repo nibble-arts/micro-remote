7d80c18760486c6a2bc9e3371d630d2614a5791e
unknown <thomas.winkler@w7wk1910.tmw.ac.at>
2017-05-15 12:58:20 +0200

    Signed-off-by: unknown <thomas.winkler@w7wk1910.tmw.ac.at>

---
+++ microRemote_Sender_V1.ino/microRemote_Sender_V1.ino.ino
@@ -0,0 +1,557 @@
+/*
+* Follow Focus sender
+* v0.2
+* 2016-12-16 by Thomas H Winkler
+* <thomas.winkler@iggmp.net>
+* 
+* using RF24 library by J. Coliz <maniacbug@ymail.com>
+* using SoftwareServo library
+*/
+
+/*
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+*/
+
+/*
+* input
+* A3 = Focus
+* A4 = FStop
+* A5 = Zoom
+* D3 = Switch1
+* D4 = Switch2
+* D5 = Switch3
+* 
+* output
+* D0 = connection status
+* D1 = power low
+* D6 = status 1
+* D9 = status 2
+* D10 = status 3
+
+for RF24 pinning refer to the text file
+*/
+
+#include <SPI.h>
+#include <RF24.h>
+#include <printf.h>
+#include <EEPROM.h>
+
+/****************** User Config ***************************/
+/* analog inputs */
+#define FOCUS A3
+#define FSTOP A4
+#define ZOOM A5
+
+/* digital input switch to gnd */
+#define SW1 3
+#define SET 4
+#define ENTER 5
+
+/* digital LED output with 1k resistor */
+#define STAT1 6
+#define STAT2 9
+#define STAT3 10
+
+#define RADIO 0
+#define PWR 1
+
+
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);
+/**********************************************************/
+
+byte addresses[][6] = {"1Node","2Node"};
+
+struct DATA {
+  bool sw1;
+  bool sw2;
+  bool sw3;
+  int focus;
+  int fstop;
+  int zoom;
+  unsigned long time;
+};
+
+#define MAX 3
+
+int maxStore = MAX;
+DATA autoStore[MAX];
+int autoVal;
+
+bool recallStat;
+int recallVal;
+
+unsigned long blinkTime;
+bool blinkStatus = LOW;
+
+unsigned long noBlinkTime;
+bool noBlinkStatus = LOW;
+
+
+/* value array */
+int value[8];
+
+
+/**********************************************************/
+/**********************************************************/
+void setup() {
+  
+//  Serial.begin(115200);
+//  Serial.println(F("RF24 follow focus"));
+  
+  // init RF24 radio transceiver
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setChannel(108);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.setRetries(15, 15);
+  
+  // Open a writing and reading pipe with opposite addresses
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1,addresses[0]);
+  
+  // Start the radio listening for data
+  radio.startListening();
+
+//******************
+//DEBUG show details
+//printf_begin();
+//radio.printDetails();
+
+  autoVal = 1;
+  recallVal = 1;
+  recallStat = false;
+  
+
+  // init IO
+  pinMode(SW1, INPUT_PULLUP);
+//  pinMode(SW2, INPUT_PULLUP);
+//  pinMode(SW3, INPUT_PULLUP);
+
+  pinMode(SET, INPUT_PULLUP);
+  pinMode(ENTER, INPUT_PULLUP);
+
+  pinMode(RADIO, OUTPUT);
+  pinMode(PWR, OUTPUT);
+
+  digitalWrite(RADIO, LOW);
+  digitalWrite(PWR, LOW);
+
+  pinMode(STAT1, OUTPUT);
+  pinMode(STAT2, OUTPUT);
+  pinMode(STAT3, OUTPUT);
+
+  digitalWrite(STAT1, LOW);
+  digitalWrite(STAT2, LOW);
+  digitalWrite(STAT3, LOW);
+
+}
+
+
+/**********************************************************/
+/**********************************************************/
+void loop() {
+
+  DATA current = scan();
+  transmit(current);
+
+  unsigned long setTime = micros();
+
+  /* enter auto mode */
+  if (checkSet() == -1) {
+    autoMode();
+  }
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* set auto data */
+void autoMode() {
+  
+  int button;
+
+  digitalWrite(RADIO, LOW);
+  statusBlink(0,1,300);
+
+  /* get data from EEPROM */
+
+
+
+
+
+
+
+  //Serial.println(autoStore);
+  
+  /* wait for setup button release */
+  while (checkSet() != 0) {}
+
+
+  /* setup loop */
+  while (true) {
+
+    /* set storeVal LED */
+    statusSet(autoVal);
+
+    /* send stored data */
+    DATA current = autoStore[autoVal-1];
+    transmit(current);
+    
+
+    /* enter edit mode */
+    if (checkEnter() == -1) {
+      edit();
+    }
+
+    /* exit or incenent */
+    button = checkSet();
+
+    if (button) {
+      if (button == -1) {
+        statusBlink(0,1,300);
+        return;
+      }
+  
+      /* toggle position */
+      else {
+        incStore();
+      }
+    }
+  }
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* save value to auto store */
+void edit() {
+  
+  int button;
+
+  digitalWrite(RADIO, LOW);
+  statusClear(0);
+
+  while (checkEnter());
+  
+  while (true) {
+
+    /* blink LED display */
+    if (millis() - blinkTime > 500) {
+
+      blinkTime = millis();
+
+      if (blinkStatus == LOW) {
+        blinkStatus = true;
+        statusSet(autoVal);
+      }
+      else {
+        blinkStatus = false;
+        statusClear(autoVal);
+      }
+    }
+
+
+    /* get and send values */
+    DATA current = scan();
+    transmit(current);
+    
+
+    /* save value or exit */
+    button = checkEnter();
+   
+    if (button) {
+
+      digitalWrite(RADIO, LOW);
+
+      /* save value */
+      if (button != -1) {
+        autoStore[autoVal-1] = current;
+        statusBlink(autoVal,2,150);
+        /* write data to EEPROM */
+      }
+      else {
+        statusBlink(0,1,300);
+        while (checkEnter());
+        break;
+      }
+    }
+
+    /* increment */
+    if (checkSet()) {
+      incStore();
+    }
+  }
+}
+
+
+/**********************************************************/
+/* check SET button
+ *  
+ *  true if longer than 1,5 seconds
+ */
+int checkSet() {
+  
+  if (!digitalRead(SET)) {
+    unsigned long current = micros();
+
+    while (!digitalRead(SET)) { 
+ 
+      if (micros() - current > 1500000) {
+        return -1;
+      }
+
+    }
+  }
+  else {
+    return 0;
+  }
+
+  return 1;
+}
+
+
+/**********************************************************/
+/* check SET button
+ *  
+ *  true if longer than 1,5 seconds
+ */
+int checkEnter() {
+  
+  if (!digitalRead(ENTER)) {
+    unsigned long current = micros();
+
+    while (!digitalRead(ENTER)) { 
+ 
+      if (micros() - current > 1500000) {
+        return -1;
+      }
+
+    }
+  }
+  else {
+    return 0;
+  }
+
+  return 1;
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* transmit data */
+void transmit(DATA data) {
+
+  radio.stopListening();
+
+  /* get send time for connection test */
+  unsigned long start_time = micros();
+  data.time = start_time;
+
+
+  value[0] = map(data.focus, 0, 1024, 0, 100);
+  value[1] = map(data.fstop, 0, 1024, 0, 100);
+  value[2] = map(data.zoom, 0, 1024, 0, 100);
+  value[3] = 1024;
+
+  value[4] = 0;
+  value[5] = 0;
+  value[6] = 0;
+  value[7] = 0;
+
+
+  /* send data */
+  if (!radio.write( &value, sizeof(int[8]) )){
+
+
+
+//  if (!radio.write( &data, sizeof(DATA) )){
+//    Serial.println(F("*** send failed"));
+
+    /* blink RADIO LED */
+//    digitalWrite(RADIO, LOW);
+
+   /* blink LED display */
+    if (millis() - noBlinkTime > 500) {
+
+      noBlinkTime = millis();
+
+      if (noBlinkStatus == LOW) {
+        noBlinkStatus = true;
+      }
+      else {
+        noBlinkStatus = false;
+      }
+
+      digitalWrite(RADIO,noBlinkStatus);
+    }
+
+  }
+  else
+    digitalWrite(RADIO, HIGH);
+
+
+  if (!data.sw1) {
+    radio.startListening();
+
+    while (!radio.available()) {  
+      if (micros() - start_time > 10000) {
+//        digitalWrite(PWR, LOW);
+        break;
+      }
+    }
+    
+    if (radio.available()) {
+      float power;
+      radio.read( &power, sizeof(float) );
+
+      digitalWrite(PWR, HIGH);
+    }
+  }
+  else {
+    digitalWrite(PWR, LOW);
+  }
+
+
+ 
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* scan input and return DATA struct */
+DATA scan () {
+  DATA io;
+
+  io.focus = analogRead(FOCUS);
+  io.fstop = analogRead(FSTOP);
+  io.zoom = analogRead(ZOOM);
+
+  io.sw1 = !digitalRead(SW1);
+//  io.sw2 = !digitalRead(SW2);
+//  io.sw3 = !digitalRead(SW3);
+
+  return io;
+}
+
+
+/**********************************************************/
+/* iterate storeVal */
+void incStore() {
+  if (autoVal == maxStore)
+    autoVal = 1;
+  else
+    autoVal++;
+}
+
+
+/**********************************************************/
+/* iterate storeVal */
+void incRecall() {
+  if (recallVal == maxStore)
+    recallVal = 1;
+  else
+    recallVal++;
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* set status LED 1-3
+ *  0 => set all LEDs
+ */
+void statusSet(int nr) {
+
+  statusClear(0);
+  
+  switch(nr) {
+    case 1:
+      digitalWrite(STAT1,HIGH);
+      break;
+    case 2:
+      digitalWrite(STAT2,HIGH);
+      break;
+    case 3:
+      digitalWrite(STAT3,HIGH);
+      break;
+    default:
+      digitalWrite(STAT1,HIGH);
+      digitalWrite(STAT2,HIGH);
+      digitalWrite(STAT3,HIGH);
+      break;
+  }
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* clear status LED 1-3
+ *  0 => clear all LEDs
+ */
+void statusClear(int nr) {
+
+  switch(nr) {
+    case 1:
+      digitalWrite(STAT1,LOW);
+      break;
+    case 2:
+      digitalWrite(STAT2,LOW);
+      break;
+    case 3:
+      digitalWrite(STAT3,LOW);
+      break;
+    default:
+      digitalWrite(STAT1,LOW);
+      digitalWrite(STAT2,LOW);
+      digitalWrite(STAT3,LOW);
+      break;
+  }
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* blink LED nr, cnt times with speed
+ *  0 => blink all LEDs
+ */
+void statusBlink(int nr, int cnt, int speed) {
+  
+  for (int i=0; i < cnt; i++) {
+    statusSet(nr);
+    delay(speed);
+
+    statusClear(nr);
+    delay(speed);
+  }
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* read data from EEPROM
+ *   return DATA struct
+ */
+
+DATA getData(void) {
+
+  DATA data;
+
+  return data;
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* write data to EEPROM
+ *   DATA struct
+ */
+
+void writeData(void) {
+  
+}
+
---
+++ microRemote_receiver.ino/microRemote_receiver.ino.ino/microRemote_receiver.ino.ino.ino
@@ -0,0 +1,288 @@
+/*
+* Follow Focus receiver
+* v0.2
+* 2016-12-16 by Thomas H Winkler
+* <thomas.winkler@iggmp.net>
+* 
+* using RF24 library by J. Coliz <maniacbug@ymail.com>
+*/
+
+/*
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+*/
+
+/*
+* input
+* A0 = battery voltage control
+* 
+* output
+* D2 = S-Bus
+* D3 = PWM servo Focus
+* D4 = PWM servo FStop
+* D5 = PWM servo Zoom
+*
+* D10 = connection status
+
+for RF24 pinning refer to the text file
+*/
+
+#include <SPI.h>
+#include <RF24.h>
+#include <printf.h>
+
+#include <Servo.h>
+
+/****************** PIN Config ***************************/
+/*                            
+ * PWM servo output
+ */
+#define FOCUS 5
+#define FSTOP 6
+#define ZOOM 9
+
+/* digital input switch to gnd */
+#define SW1 3
+
+/* digital LED output with 1k resistor to gnd */
+#define REC 4
+#define STATUS 10
+
+
+/***************************************************
+ * S-Bus definitions
+ */
+#include <BMC_SBUS.h>
+
+#define RECchannel 1
+#define IRISchannel 2
+#define FOCUSchannel 3
+#define ZOOMchannel 4
+#define ISOchannel 5
+#define SECTORchannel 6
+#define COLORchannel 7
+#define AUDIOchannel 8
+
+const int sbusMID         = 1024;   //neutral val
+const int sbusLOW         = 0;      //low switch val
+const int sbusHIGH        = 2000;   //high switch val
+
+const int sbusWAIT = 4;      //frame timing delay in msecs
+
+BMC_SBUS mySBUS;
+
+
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(8,7);
+
+/* Servo definition */
+Servo FocusServo;
+Servo FstopServo;
+Servo ZoomServo;
+
+/**********************************************************/
+/* rf24 identification data definition */
+byte addresses[][6] = {"1Node","2Node"};
+
+
+unsigned long timeout;
+bool blinkStatus;    
+
+unsigned long lowTime;
+bool lowStatus;    
+
+bool online;
+float power;
+
+
+/* value array */
+int value[8];
+
+
+
+/**********************************************************/
+/* setup routine                                          */
+void setup() {
+
+  Serial.begin(9600);
+
+  /* init S-BUS */
+  mySBUS.begin();
+
+
+/**************************/
+/* init radio             */
+  radio.begin();
+
+//TODO make channel selectable
+
+  radio.setChannel(108);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.setRetries(15, 15);
+  
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
+
+  
+  // Start the radio listening for data
+  radio.startListening();
+
+
+/*  if (radio.failureDetected)
+    Serial.println("failure");
+  else
+    Serial.println("no failure");
+
+
+  if (radio.isValid())
+    Serial.println("is valid RF24L01");
+  else
+    Serial.println("no chip found");
+*/
+
+/**************************/
+/* init servo             */
+  FocusServo.attach(FOCUS);
+  FstopServo.attach(FSTOP);
+  ZoomServo.attach(ZOOM);
+
+  // init IO
+  pinMode(SW1, OUTPUT);
+
+  pinMode(STATUS, OUTPUT);
+  digitalWrite(STATUS, LOW);
+
+  pinMode(REC, OUTPUT);
+  digitalWrite(REC, LOW);
+
+  online = false;
+
+
+/*  digitalWrite(STATUS, HIGH);
+  delay(200);
+  digitalWrite(STATUS, LOW);
+  delay(200);
+  digitalWrite(STATUS, HIGH);
+  delay(200);
+  digitalWrite(STATUS, LOW);
+  delay(200);*/
+
+}
+
+
+/**********************************************************/
+/* main loop                                              */
+void loop() {
+
+  int focusVal;
+  int fstopVal;
+  int zoomVal;
+
+
+  /* get radio data */
+  if(radio.available()){
+
+    radio.read( &value, sizeof(int[8]) );
+    timeout = micros();
+
+    digitalWrite(STATUS, HIGH);
+
+    transmit();
+  }
+
+  /* if no radio, check for timeout */
+  else {
+
+    /* blink LED */
+    if (micros()-timeout > 500000) {
+      timeout = micros();
+
+      if (blinkStatus == LOW)
+        blinkStatus = HIGH;
+      else
+        blinkStatus = LOW;
+
+      digitalWrite(STATUS, blinkStatus);
+
+
+      /* stop camera on transmit lost */
+//      digitalWrite(SW1, LOW);
+    }
+  }
+
+}
+
+
+/**********************************************************/
+/**********************************************************/
+/* transmit data
+ *    CABLE: true => send SBUS
+ *           false => send RF
+ */
+void transmit() {
+
+
+  /* analog channels */
+  mySBUS.Servo(FOCUSchannel, map(value[0],0,100,0,2047)); /* focus */
+  mySBUS.Servo(IRISchannel, map(value[1],0,100,0,2047)); /* iris */
+  mySBUS.Servo(ZOOMchannel, map(value[2],0,100,0,2047)); /* zoom */
+  mySBUS.Servo(AUDIOchannel, map(value[3],0,100,0,2047)); /* audio */
+
+  mySBUS.Update();
+  mySBUS.Send();
+  delay(sbusWAIT);
+
+  /* digital channels */
+  stepChannel(4,ISOchannel);
+  stepChannel(5,SECTORchannel);
+  stepChannel(6,COLORchannel);
+  stepChannel(7,RECchannel);
+
+
+  /* reset digital data */
+  value[4] = 0;
+  value[5] = 0;
+  value[6] = 0;
+  value[7] = 0;
+
+}
+
+
+/***********************************************
+ * send step sequence
+ */
+void stepChannel (int val, int channel) {
+
+  if (value[val] != 0) {
+
+    if (value[val] > 0) {
+
+      mySBUS.Servo(channel, sbusMID);
+      mySBUS.Update();
+      mySBUS.Send();
+
+      delay(sbusWAIT);
+  
+      mySBUS.Servo(channel, sbusHIGH);
+      mySBUS.Update();
+      mySBUS.Send();
+    }
+
+    else {
+      mySBUS.Servo(channel, sbusMID);
+      mySBUS.Update();
+      mySBUS.Send();
+
+      delay(sbusWAIT);
+  
+      mySBUS.Servo(channel, sbusLOW);
+      mySBUS.Update();
+      mySBUS.Send();
+    }
+
+    delay(500);
+  }
+}
+
